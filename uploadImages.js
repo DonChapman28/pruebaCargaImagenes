b626daf8d7759830f4f13723baa158941b1f3986
eSponjosa <e.rojascisterna@outlook.com>
2024-06-08 21:27:03 -0400

    Update uploadImages.js

--- uploadImages.js
+++ uploadImages.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
-const BASE_URL = `https://pruebacargaimagenes.onrender.com:${PORT}`;
+const BASE_URL = `https://pruebacargaimagenes.onrender.com:`;
 // Habilitar CORS
 app.use(cors());
 // Configurar almacenamiento de multer
