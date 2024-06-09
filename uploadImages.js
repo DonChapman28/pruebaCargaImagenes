8808594a50456cc8adb42238f49918ee0921dcfd
eSponjosa <e.rojascisterna@outlook.com>
2024-06-08 20:38:42 -0400

    update carga imagenes

--- uploadImages.js
+++ uploadImages.js
@@ -2,40 +2,52 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
-
+const BASE_URL = `http://localhost:${PORT}`;
+// Habilitar CORS
+app.use(cors());
 // Configurar almacenamiento de multer
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadDir = './uploads';
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir);
-    }
-    cb(null, uploadDir);
+  destination: function (req, file, cb) {
+    cb(null, './uploads'); // Directorio donde se guardarán los archivos
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Agrega timestamp al nombre del archivo
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo en el servidor
   }
 });
 
-const upload = multer({ storage });
 
+const upload = multer({ storage: storage });
 // Ruta para subir imágenes
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post('/uploads',upload.single('imageBase64'), (req, res) => {
+ //valida que los datos de file sean verdadeeros
   if (!req.file) {
-    return res.status(400).send('No se subió ningún archivo.');
+    return res.status(400).send('No se recibió ninguna imagen.');
   }
-  const fileUrl = `http://localhost:${PORT}/uploads/${req.file.filename}`;
+
+  const { originalname, mimetype, path: filePath } = req.file;
+  const validTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+  //valida tipos
+  if (!validTypes.includes(mimetype.toLowerCase())) {
+    // Eliminar el archivo si no es válido
+    fs.unlinkSync(filePath);
+    return res.status(400).send('Tipo de imagen no permitido.');
+  }
+
+  //crea constante con la url para mandar a la base de datos
+  const fileUrl = `${BASE_URL}/uploads/${path.basename(filePath)}`;
+  console.log('Carga exitosa:', originalname);
   res.status(200).json({ url: fileUrl });
 });
+
 
 //rutas
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-/* app.use('/public', express.static(path.join(__dirname, 'public')));
- */
 // inicio
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
@@ -60,6 +72,6 @@ app.get('/images', (req, res) => {
   });
   
 
-app.listen(PORT, () => {
-  console.log(`Servidor funcionando en http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Servidor funcionando en ${BASE_URL}`);
+  });
